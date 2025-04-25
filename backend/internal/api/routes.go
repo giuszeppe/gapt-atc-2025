db94b538eefa9151b657bad849d48ba48213796b
Giuseppe <gdmr.work@gmail.com>
2025-04-25 16:21:21 +0200

    remove unused params

--- backend/internal/api/routes.go
+++ backend/internal/api/routes.go
@@ -14,16 +14,8 @@ func addRoutes(
 	logger *slog.Logger,
 	tokenStore stores.Store[string],
 	userStore stores.UserStore,
-	// config              Config,
-	// tenantsStore        *TenantsStore,
-	// commentsStore       *CommentsStore,
-	// conversationService *ConversationService,
-	// chatGPTService      *ChatGPTService,
 	// authProxy           *authProxy,
 ) {
-	// mux.Handle("/api/v1/", handleTenantsGet(logger, tenantsStore))
-	// mux.Handle("/oauth2/", handleOAuth2Proxy(logger, authProxy))
-	// mux.HandleFunc("/healthz", handleHealthzPlease(logger))
 	mux.Handle("/login", services.HandleLoginService(logger, userStore, tokenStore))
 	mux.Handle("/test", middlewares.Auth(services.HandleTestService(logger), tokenStore))
 	mux.Handle("/", http.NotFoundHandler())

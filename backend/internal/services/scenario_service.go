16045d14a5cbfd138c99bb74d23c710fa07b007b
Giuseppe <gdmr.work@gmail.com>
2025-05-03 16:54:01 +0200

    add post simulation endpoint to begin simulating

--- backend/internal/services/scenario_service.go
+++ backend/internal/services/scenario_service.go
@@ -3,7 +3,6 @@ package services
 import (
 	"log/slog"
 	"net/http"
-
 	"github.com/giuszeppe/gatp-atc-2025/backend/internal/encoder"
 	"github.com/giuszeppe/gatp-atc-2025/backend/internal/stores"
 )
@@ -21,9 +20,29 @@ func HandleGetScenario(logger *slog.Logger, scenarioStore stores.ScenarioStore) 
 		},
 	)
 }
+
+type PostScenarioRequest struct {
+	Id int
+}
+
 func HandlePostSimulation(logger *slog.Logger, scenarioStore stores.ScenarioStore) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			// get simulation params
+			data, err := encoder.Decode[PostScenarioRequest](r)
+			if err != nil {
+				encoder.EncodeError(w, http.StatusBadRequest, nil, err.Error())
+				return
+			}
+
+			// fetch scenario steps
+			steps, err := scenarioStore.GetScenarioStepsForId(data.Id)
+			if err != nil {
+				encoder.EncodeError(w, 500, err, err.Error())
+			}
+
+			// return steps
+			encoder.Encode(w, r, 200, steps)
 
 		},
 	)
--- backend/internal/stores/scenario_store.go
+++ backend/internal/stores/scenario_store.go
@@ -6,10 +6,11 @@ import (
 )
 
 type Scenario struct {
-	ID    int
-	Name  string
-	Type  string
-	Steps []Step
+	ID            int
+	Name          string
+	Type          string
+	Steps         []Step
+	ExtendedSteps []Step
 }
 
 type Step struct {
@@ -63,4 +64,39 @@ func (s *ScenarioStore) View(scenarioType string) ([]Scenario, error) {
 	}
 
 	return scenarios, nil
+}
+func (s *ScenarioStore) GetScenarioStepsForId(scenarioId int) ([][]Step, error) {
+	stmt, err := s.db.Prepare(`
+        SELECT st.idx, st.text, st.role, est.idx, est.text, est.role
+        FROM scenarios s
+        LEFT JOIN steps st on s.id=st.scenario_id
+        LEFT JOIN extended_steps est on s.id=est.scenario_id AND est.idx = st.idx
+        WHERE s.id= ?;
+        `)
+	if err != nil {
+		return [][]Step{}, err
+	}
+	defer stmt.Close()
+
+
+	rows, err := stmt.Query(scenarioId)
+	if err != nil {
+		return [][]Step{}, err
+	}
+	defer rows.Close()
+
+    res := [][]Step{[]Step{}, []Step{}}
+
+	for rows.Next() {
+		var step Step
+		var extendedStep Step
+		if err := rows.Scan(&step.Index, &step.Text, &step.Role, &extendedStep.Index, &extendedStep.Text, &extendedStep.Role); err != nil {
+			return [][]Step{}, err
+		}
+        res[0] = append(res[0], step)
+        res[1] = append(res[1], extendedStep)
+
+	}
+
+	return res, nil
 }

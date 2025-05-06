98346c2d356d7d285a37efef1786087475afbc92
Giuseppe <gdmr.work@gmail.com>
2025-05-06 22:30:56 +0200

    change id column type from int to integer in transcripts table

--- backend/internal/db/ddl/transcripts.sql
+++ backend/internal/db/ddl/transcripts.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS transcripts;
 
 CREATE TABLE IF NOT EXISTS transcripts (
-    id int primary key,
+    id integer primary key,
     text text,
     role varchar(255),
     simulation_id int

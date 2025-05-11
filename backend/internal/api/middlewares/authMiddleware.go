368bf24202cb0624e22a1f1a22be2ea122961c82
Giuseppe <gdmr.work@gmail.com>
2025-05-11 21:25:53 +0200

    add auth guard for all routes

--- backend/internal/api/middlewares/authMiddleware.go
+++ backend/internal/api/middlewares/authMiddleware.go
@@ -7,7 +7,7 @@ import (
 	"github.com/giuszeppe/gatp-atc-2025/backend/internal/stores"
 )
 
-func Auth(h http.Handler, tokenStore stores.TokenStore) http.Handler {
+func Auth(h http.Handler, tokenStore *stores.TokenStore) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		v := r.Header.Get("Authorization")
 		_, err := tokenStore.GetUserByToken(v)
